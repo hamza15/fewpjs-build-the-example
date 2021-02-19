c879b657a1254af0f93b975c5e1ea0a7c403ec5e
hamza15 <sardan5@vt.edu>
2021-02-19 14:04:20 -0500

    commit tree ce5bd6a20bc5b287d74e3c431a107bfd3517be0f

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.querySelector('#modal')
+modal.setAttribute('class', 'hidden');
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  
 
+  const hearts = document.querySelectorAll('span.like-glyph')
+  console.log(hearts)
 
+  for (const heart of hearts){
+    heart.addEventListener('click', (e) => {
+      mimicServerCall()
+      .then(() => {
+        if (heart.innerHTML == EMPTY_HEART) {
+          heart.innerHTML = FULL_HEART
+          heart.setAttribute('class', 'activated-heart')
+        } else {
+          heart.innerHTML = EMPTY_HEART
+          heart.className = "like-glyph"
+        }
+      })
+      .catch(error => {
+        modal.classList.remove("hidden");
+        const modalMessage = document.querySelector("#modal-message")
+        modalMessage.innerText = error
+        setTimeout(() => {
+          modal.setAttribute('class', 'hidden')
+        }, 5000)
+      })
+    })
+  }
 
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
